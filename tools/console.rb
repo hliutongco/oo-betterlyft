ce701db14f1ee1dfbc91dfc23245ce506e25e0ed
Helen Liutongco <liutongcoh@gmail.com>
2018-09-06 16:52:19 -0400

    Add all the code

--- tools/console.rb
+++ tools/console.rb
@@ -5,6 +5,13 @@ def reload
 end
 
 
-# Put your variables here~!
+helen = Passenger.new("Helen")
+ruby = Passenger.new("Ruby")
+santiago = Driver.new("Santiago")
+jackie = Driver.new("Jackie")
+ride1 = Ride.new(jackie, ruby, 1.0)
+ride2 = Ride.new(santiago, helen, 101.0)
+ride3 = Ride.new(santiago, helen, 101.0)
 
+# santiago.passenger_names
 binding.pry
